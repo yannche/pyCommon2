d706eedeae1126d32263bf8dfa9e4fa4150b1c5e
yannche <yann_che2@yahoo.fr>
2018-01-08 10:32:21 +0100

    Update mise_a_jour.bash

--- mise_a_jour.bash
+++ mise_a_jour.bash
@@ -2,12 +2,8 @@
 
 echo " -+-+- Veuillez attendre une dizaine de secondes que la mise a jour se fasse -+-+-"
 
-cd ~/Code/pySpriteWorld
+cd ~/pyCommon2
 git fetch origin
 git reset --hard origin/master
 
-cd ~/Code/pyCommon
-git fetch origin
-git reset --hard origin/master
-
-~/Code/pyCommon/.apres_maj
+~/pyCommon/apres_maj
